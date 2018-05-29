ec8c078b52e5fe60f30b405b556265ce06419129
TessACraig89 <tesscraig.webdev@gmail.com>
2018-05-29 18:04:02 -0500

    WIP walkthrough lesson

---
+++ main.js
@@ -0,0 +1,23 @@
+/// Jquery
+const container = document.querySelector("#container");
+
+console.log(container);
+
+
+const secondImg = document.querySelector("#second-img");
+
+console.log(secondImg);
+
+
+const infoElements = document.querySelectorAll(".info");
+
+console.log(infoElements);
+
+
+const firstP = document.querySelector("#first-p");
+
+firstP.innerHTML = '<strong>Put your hands where I can see \'em, so they look like 12pm</strong>'
+
+const newDiv = document.createElement('div');
+
+console.log(newDiv);
